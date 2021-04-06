a53bef22bf707ab3a0b4cbfefe3aadbabb845a64
skadevare <70136435+skadevare@users.noreply.github.com>
2021-04-06 12:09:38 +0200

    Update Shodan-ip-list.sh

--- Shodan-ip-list.sh
+++ Shodan-ip-list.sh
@@ -6,10 +6,10 @@ echo " - Shodan multiple ips from file-list.sh - "
 echo "###########################################"
 echo "Input filelist:"
 read varfilelist
-echo "Save output to:
+echo "Save output to:"
 read varoutput
 echo "start"
 while read p
 do
         shodan host ${p}>>$varoutput
-done<$filelist
+done<$varfilelist
