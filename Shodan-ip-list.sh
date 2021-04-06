52cca66f4e8b9a336fff1b12c103d158f1ba756c
skadevare <70136435+skadevare@users.noreply.github.com>
2021-04-06 12:06:13 +0200

    Create Shodan-ip-list.sh

---
+++ Shodan-ip-list.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+echo "###########################################"
+echo " - Shodan multiple ips from file-list.sh - "
+echo "###########################################"
+echo "Input filelist:"
+read varfilelist
+echo "Save output to:
+read varoutput
+echo "start"
+while read p
+do
+        shodan host ${p}>>$varoutput
+done<$filelist
